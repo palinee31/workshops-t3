b76cfe25f8f5eaa524bfbe2a675231477be49e44
palinee31 <palinee.w@ku.th>
2025-10-03 16:49:19 +0700

    [FIX] :ambulance:

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+export default {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    transform: {
+      '^.+\\.[tj]sx?$': ['ts-jest', {
+        useESM: true,
+      }],
+    },
+    moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  };
